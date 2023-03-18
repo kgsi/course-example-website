8eff461b199a6147f51b78ce5956a5f6d0c2d559
shinichi.kogiso <kuusai1998@gmail.com>
2023-03-19 00:24:57 +0900

    テストアップ

--- complete_sample/.eslintrc.cjs
+++ complete_sample/.eslintrc.cjs
@@ -1,14 +1,14 @@
 module.exports = {
   env: {
     browser: true,
-    es2021: true,
+    es2021: true
   },
-  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'plugin:storybook/recommended'],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 'latest',
-    sourceType: 'module',
+    sourceType: 'module'
   },
   plugins: ['@typescript-eslint'],
-  rules: {},
-}
+  rules: {}
+};
